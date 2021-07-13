102557a4d813f8c2464ebe697278693732d47fda
Simon Templer <sajtempler@gmail.com>
2021-07-13 15:30:31 +0200

    feat(3d): add 3d point

---
+++ src/3d/point3d.ts
@@ -0,0 +1,19 @@
+/** Class representing a 3d point. */
+export class Point3D {
+
+    /**
+     * Create a 2d point. Defaults to (0, 0)
+     * @param {number} x - The x value.
+     * @param {number} y - The y value.
+     * @param {number} y - The y value.
+     */
+    constructor(
+        public x: number = 0,
+        public y: number = 0,
+        public z: number = 0
+    ) {}
+
+    static fromArray([x, y, z]: [number, number, number]): Point3D {
+        return new Point3D(x, y, z);
+    }
+}
